275ef61130753b4bbaf6ca5c9434b189b99c3249
Vasilena Stanoyska <vasilena.stanoyska@sap.com>
2026-04-14 12:20:32 +0300

    feat(04-05-02): register recipe slice in Redux store
    - Import recipeReducer from recipeSlice
    - Add recipes: recipeReducer to store configuration
    - Recipe state now accessible at state.recipes

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
+import recipeReducer from './slices/recipeSlice';
 
 const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    recipes: recipeReducer
   }
 });
 
