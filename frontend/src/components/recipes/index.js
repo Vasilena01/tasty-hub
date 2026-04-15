8620b4b08aa9fd9e9651e8e6d2726fb733a827ae
Vasilena Stanoyska <vasilena.stanoyska@sap.com>
2026-04-15 10:28:15 +0300

    fix(04-05): preserve existing exports in recipes index.js
    - Add new component exports while keeping existing RecipeForm, ImageUpload, IngredientList
    - Prevents breaking existing imports from previous plans

--- frontend/src/components/recipes/index.js
+++ frontend/src/components/recipes/index.js
@@ -1,3 +1,8 @@
 export { default as RecipeForm } from './RecipeForm';
 export { default as ImageUpload } from './ImageUpload';
 export { default as IngredientList } from './IngredientList';
+export { default as RecipeCard } from './RecipeCard';
+export { default as RecipeGrid } from './RecipeGrid';
+export { default as FilterSidebar } from './FilterSidebar';
+export { default as SearchBar } from './SearchBar';
+export { default as Pagination } from './Pagination';
